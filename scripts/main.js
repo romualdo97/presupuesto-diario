e6a0dfe01072d9963afc8c1374321427d363ee8d
Jose Romualdo Villalobos Perez <jorovipe@gmail.com>
2016-04-20 18:45:51 -0500

    La aplicacion ahora tiene la funcionalidad de historial

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,20 @@
 // 245571 durante 22dias
 var fondos = 0, // dinero con que se cuenta
 	numeroDias = 0; // numero de dias que se debe administrar el fondo
+
 var hoy = new Date(),
 	dd = hoy.getDate(),
 	mm = hoy.getMonth() + 1,
 	yyyy = hoy.getFullYear(),
 	fecha_formato_hoy = dd + "/" + mm + "/" + yyyy;
+
 // html elements
 var fondos_element = document.getElementById("fondos"),
 	numDias_element = document.getElementById("numDias"),
 	cuantoDeberiaGastar_element = document.getElementById("cuantoGastar"),
-	cuantosDiasFaltan_element = document.getElementById("cuantosDias");
+	cuantosDiasFaltan_element = document.getElementById("cuantosDias")
+	historialDeGastos_element = document.getElementById("historialGastos");
+
 
 if (localStorage.fondos) 
 {
@@ -42,7 +46,7 @@ function setfondos(value)
 {
 	if (value < 0) return;
 	fondos = value;
-	console.log("fondos establecidos");
+	console.log("Fondos establecidos");
 	saveChanges();
 }
 
@@ -98,3 +102,36 @@ function saveChanges()
 	localStorage.numeroDias = numeroDias;
 	mostrarCuantoDeberiaGastar();
 }
+
+// HISTORIAL DE GASTOS
+function saveOnHistory( value )
+{
+	var gastos = JSON.parse( localStorage.historialGastos );
+
+	gastos.push(value);
+	console.log("Guardado");
+	console.log("Preparando pal server.. " + gastos);
+
+	localStorage.historialGastos = JSON.stringify( gastos );
+	console.log("En el server.. " + JSON.parse( localStorage.historialGastos ) );
+}
+
+function resetHistorial()
+{
+	localStorage.historialGastos = JSON.stringify([]);
+}
+
+function mostrarHistorialDeGastos()
+{	
+	gastosInServer = JSON.parse( localStorage.historialGastos );
+	console.log("Probando persistencia... ");
+	console.log(gastosInServer)
+	console.log(gastosInServer.length)
+
+	for (i = 0; i < gastosInServer.length; i++) {
+		var gasto_element = document.createElement("LI");
+		historialDeGastos_element.appendChild(gasto_element);
+		gasto_element.innerHTML = gastosInServer[i];
+	}
+}
+mostrarHistorialDeGastos();
